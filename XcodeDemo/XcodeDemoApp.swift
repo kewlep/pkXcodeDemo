9b8e0d3617be7c971c657b2083ea18b008a7aebe
kewlep <kewlep@gmail.com>
2023-10-05 17:42:52 +1100

    Initial Commit

---
+++ XcodeDemo/XcodeDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeDemoApp.swift
+//  XcodeDemo
+//
+//  Created by Philip Kewley on 5/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct XcodeDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
